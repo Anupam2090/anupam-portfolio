39e4f781599c7cefbb055501ab53a76f34b1e6e4
Anupam2090 <anupam06122001@gmail.com>
2026-03-16 09:47:03 +0530

    git,linked in setup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,39 @@
 import React from 'react'
-import { FaGithub, FaLinkedin } from 'react-icons/fa'
 import './Navbar.css'
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
 
-const Navbar = () => (
-  <nav className="navbar">
-    <h1 className="logo">Anupam Bera</h1>
-    <div className="nav-links">
-      <a href="#about">About</a>
-      <a href="#skills">Skills</a>
-      <a href="#projects">Projects</a>
-      <a href="#contact">Contact</a>
-    </div>
-    <div className="socials">
-      <a href="[github.com](https://github.com/Anupam2090)">
-        <FaGithub />
-      </a>
-      <a href="[linkedin.com](https://linkedin.com/in/anupam-bera)">
-        <FaLinkedin />
-      </a>
-    </div>
-  </nav>
-)
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">Anupam</div>
+
+      <div className="nav-links">
+        <a href="#about">About</a>
+        <a href="#projects">Projects</a>
+        <a href="#contact">Contact</a>
+      </div>
+
+      <div className="nav-right">
+        <div className="socials">
+          <a
+            href="https://github.com/Anupam2090"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub />
+          </a>
+
+          <a
+            href="https://linkedin.com/in/anupam-bera"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin />
+          </a>
+        </div>
+      </div>
+    </nav>
+  )
+}
 
 export default Navbar
